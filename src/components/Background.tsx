f1f1b4d09a91a1678e993f56ae23221b02d8c381
Alexandre Cochet <alexandre.cochet0@gmail.com>
2024-01-08 17:55:43 +0100

    feat(app): background

---
+++ src/components/Background.tsx
@@ -0,0 +1,3 @@
+export default function Background() {
+  return <div className="absolute w-full h-[14rem] bg-banner bg-cover bg-no-repeat" />;
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        black: "#111315",
+        brown: "#1B1D1F",
+        gray: "#6F757C",
+        beige: "#BEE3CC",
+        white: "#FEF7EE",
+        yellow: "#F6C768",
+        orange: "#ED735D",
       },
+      backgroundImage: {
+        banner: "url('/bg-cafe.jpg')",
+      }
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
